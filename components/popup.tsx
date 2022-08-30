b45129b560c5fddc9a3373ab77fdf4772c38ce23
Aquilae00 <linaldi03@gmail.com>
2022-08-29 23:53:37 -0400

    images

--- components/popup.tsx
+++ components/popup.tsx
@@ -11,20 +11,30 @@ export default function Popup({data}: PopupProps): JSX.Element {
     return (
         <div className='flex justify-center items-center space-x-sm'>
             <div className='flex flex-col items-center justfiy-center p-base border rounded-md bg-gray-500 font-bold'>
-                <div className='flex gap-base'>
-                    <RiStackLine className='text-xl' />
-                    <span>Height</span>
+                <div className='flex gap-base items-center'>
+                    <img
+                        src='https://raw.githubusercontent.com/Aquilae00/FOBEE/main/assets/layer-icon.png?token=GHSAT0AAAAAABVMJH66GQ6G6XO74PB4B74MYYNRBTA'
+                        height={50}
+                        width={50}
+                    />
+
+                    <span className='text-lg'>Height</span>
                 </div>
             </div>
             <div className='flex flex-col items-center justfiy-center  p-base border rounded-md bg-gray-500 font-bold'>
-                <div className='flex gap-base'>
+                <div className='flex gap-base items-center'>
                     <img
-                        src='data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
-    AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
-        9TXL0Y4OHwAAAABJRU5ErkJggg=='
-                        alt='Red dot'
+                        src='https://raw.githubusercontent.com/Aquilae00/FOBEE/main/assets/area-floor-size-icon.png?token=GHSAT0AAAAAABVMJH66ESQCTWIRC67BFJRYYYNRFWQ'
+                        height={50}
+                        width={50}
                     />
-                    <span>Area</span>
+                    <span className='text-lg'>Area</span>
+                </div>
+                <div>
+                    <span>{Math.ceil(data.area)}</span>
+                    <span className='text-md'>
+                        m<sup>2</sup>
+                    </span>
                 </div>
             </div>
         </div>
